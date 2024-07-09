8f9baa38b67e27ad8a5f7694e98955bb8d424c8b
Rob van Hoboken <rob.on.lists@gmail.com>
2024-07-09 16:54:23 +0200

    sements select via buttons, show instead of hide
    segment selection is now via buttons
    fields are only evaluated when the button is pressed
    field specifications kept in structure, per entity type, so maintained when switching types
    instead of "hide segment" and "hide (field)" we now say "show"

--- main.py
+++ main.py
@@ -3,7 +3,12 @@ from make_stylable import make_stylable
 import time
 
 def MaybeUpper(text):
+    '''relies on global (result of a switch) var mixedcase to apply upper() to selection values'''
     return text if mixedcase else text.upper()
+
+def dictField(d,f,default=None):
+    '''conditionally return a dict member value'''
+    return d[f] if f in d else default
 
 @st.cache_data
 def get_config():
@@ -44,15 +49,26 @@ r = get_RACF(config['unload'])
 from field_spec import entity_key_selector, entity_data_selector, segment_getter, system_profile_getter
 
 @st.cache_data
-def get_segments():
-    return segment_getter(r)
-segments = get_segments()
+def get_segment_defs():
+    # create {entity : {segment: { 'table': table } } }
+    segment_defs = {'connect': {'base': {} }, 'system profile': {'base': {} } }
+    segment_names = segment_getter(r)  # {entity: {segment: table} }
+    for e in entity_data_selector.keys():
+        if e in segment_names:
+            segment_defs[e] = {s:{'table': segment_names[e][s]} for s in segment_names[e].keys()}
+        else:
+            segment_defs[e] = {'base': {} }
+    return segment_defs
+
+if 'query_state' in st.session_state:
+    query_state = st.session_state['query_state']
+else:
+    query_state = get_segment_defs()
 
 select_frame = st.sidebar
 header = st.container()
 output_frame = st.container()
 input_table = ''
-final_processing = []
 
 with select_frame:
     track('top of loop')
@@ -89,95 +105,138 @@ with select_frame:
                         select_keys.append(None)
             i += 1
         track('keys done')
-        # create a tab for each segment available, or 1 tab for the base segment
-        snames = list(segments[select_entity].keys()) if select_entity in segments else ['base']
-        (stabs) = st.tabs(snames)
-        # st.tabs does not have a "name" or "id" attribute, so we loop over the names...
-        for sindex in range(len(snames)):
-            segment = snames[sindex]
-            track('start segment tab',segment)
-            with stabs[sindex]:
-                if len(snames)==1:
-                    hide_segment = False
-                else:
-                    # initially show only base segment (sindex==0)
-                    if sindex!=0:
-                        if select_entity+'_open_'+segment not in st.session_state: st.session_state[select_entity+'_open_'+segment] = False
-                        open_segment = st.toggle('open segment tab',key=select_entity+'_open_'+segment)
-                        if not open_segment: continue  # skip segment field elements
-                    if select_entity+'_hide_'+segment not in st.session_state: st.session_state[select_entity+'_hide_'+segment] = (sindex!=0)
-                    hide_segment = st.toggle('hide output columns',key=select_entity+'_hide_'+segment)
 
-                if sindex==0:  # base segment, field names without prefix
-                    rubbish = ['RECORD_TYPE','NAME','CLASS_NAME','CLASS','ALTER_CNT','CONTROL_CNT','UPDATE_CNT','READ_CNT','LASTREF_DATE']
-                    if select_entity in entity_data_selector:  # preferred fields listed
-                        columns = entity_data_selector[select_entity]
-                    else:  # system profiles show all fields
-                        columns = [c for c in r.table(input_table).columns if c.split('_',1)[1] not in rubbish]
-                else:  # keep the prefix
-                    input_table = segments[select_entity][segment]
-                    rubbish = ['RECORD_TYPE','NAME','CLASS_NAME','CLASS']
+        # create a button for each segment available, or 1 tab for the base segment
+        query_segments = query_state[select_entity]
+        if len(query_segments.keys())==1:
+            segment = 'base'
+        else:
+            cs = make_stylable(st.container(),
+                css_key='segment-buttons',
+                css_styles="""
+                {
+                    display: flex;
+                    flex-direction: row;
+                    flex-wrap: wrap;
+                    & div {
+                        display: flex;
+                        flex-direction: row;
+                        flex-wrap: wrap;
+                        width: auto;
+                        & ~ div {
+                            width: auto;
+                        }
+                    }
+                    & div.stButton {
+                        width: auto ! important;
+                    }
+                    & div.stTooltipIcon > div:last-child {
+                        display: none;
+                    }
+                }
+                """)
+
+            with cs:
+                for segment in query_segments.keys():
+                    color = ''
+                    hide = (1-dictField(st.session_state,select_entity+'_show_'+segment,segment!='base'))
+                    ix = 2*hide + (dictField(query_segments[segment],'filters',0)==0)
+                    colors = ['green','blue','red','grey']
+                    helps = ['segment with selection criteria','segment data on output','hidden segment with selection criteria','no segment data on output']
+                    query_segments[segment]['open'] = st.button(f":{colors[ix]}[{segment}]", help=helps[ix])
+
+            segment_clicked = [s for s in query_segments.keys() if query_segments[s]['open']]
+            if len(segment_clicked)==1:
+                segment = segment_clicked[0]
+            elif select_entity+'_segment' in st.session_state:
+                segment = st.session_state[select_entity+'_segment']
+            else:
+                segment = 'base'
+            st.session_state[select_entity+'_segment'] = segment
+
+        track('start segment tab',segment)
+        cs = make_stylable(st.container(),
+            css_key='field-prompts',
+            css_styles="""
+            {
+               & div.stTextInput {
+                  margin-top: -0.4em;
+               }
+               & div.stCheckbox {
+                    min-height: 0;
+                    padding-right: 0;
+                    margin-block: -0.4em;
+                    margin-right: -1em;
+               }
+            }
+            """)
+        with cs:
+            if len(query_segments)==1:
+                show_segment = True
+            else:
+                if select_entity+'_show_'+segment not in st.session_state: st.session_state[select_entity+'_show_'+segment] = (segment=='base')
+                show_segment = st.toggle('show segment on output',key=select_entity+'_show_'+segment)
+
+            if segment=='base':  # base segment, field names without prefix
+                rubbish = ['RECORD_TYPE','NAME','CLASS_NAME','CLASS','ALTER_CNT','CONTROL_CNT','UPDATE_CNT','READ_CNT','LASTREF_DATE']
+                if select_entity in entity_data_selector:  # preferred fields listed
+                    columns = entity_data_selector[select_entity]
+                else:  # system profiles show all fields
                     columns = [c for c in r.table(input_table).columns if c.split('_',1)[1] not in rubbish]
-                hide_columns = [c for a,b,c in [c.split('_',1)+[c] for c in r.table(input_table).columns] if a==input_table and b in rubbish]
-                select_datafields = {}
-                skip_datafields = {}
+            else:  # keep the prefix
+                input_table = query_state[select_entity][segment]['table']
+                rubbish = ['RECORD_TYPE','NAME','CLASS_NAME','CLASS']
+                columns = [c for c in r.table(input_table).columns if c.split('_',1)[1] not in rubbish]
+            hide_columns = [c for a,b,c in [c.split('_',1)+[c] for c in r.table(input_table).columns] if a==input_table and b in rubbish]
+            select_datafields = {}
+            skip_datafields = {}
 
-                track('start with columns')
-                for c in columns:
-                    with make_stylable(st.container(),
-                        css_key="compound_header",
-                        css_styles="""
-                           div.stTextInput {
-                              margin-top: -0.4em
-                           }
-                           div.stCheckbox {
-                                min-height: 0;
-                                padding-right: 0;
-                                margin-block: -0.8em;
-                           }
-                        """
-                    ):
-                        c1,c2 = st.columns([5,4],vertical_alignment="center")
-                        c1.write(c.replace(input_table+'_',''))
-                        #  testing values from session_states is expensive, and default value is false anyway
-                        # if select_entity+'_hide_'+c not in st.session_state: st.session_state[select_entity+'_hide_'+c] = False
-                        # if select_entity+'_skip_'+c not in st.session_state: st.session_state[select_entity+'_skip_'+c] = False
-                        chide = c2.toggle('hide',key=select_entity+'_hide_'+c)
-                        cskip = c2.toggle('skip',key=select_entity+'_skip_'+c)
-                        cval = st.text_input(c, key=select_entity+'_value_'+c,
-                                           help='Enter pattern, \*\* for all non-empty values, \*text\* to search anywhere, or YES/NO for flag fields',
-                                           label_visibility="collapsed")
-                    if cval:
-                        if cskip:
-                            skip_datafields.update({c:MaybeUpper(cval)})
-                        else:
-                            select_datafields.update({c:MaybeUpper(cval)})
-                    if chide:
-                        hide_columns.append(c)
-                if sindex==0 and select_entity in entity_data_selector:  # base segments, for all but the system profiles
-                    c = 'irrelevant fields'
-                    c1,c2 = st.columns([5,4],vertical_alignment="center")
-                    c1.write(c)
-                    if select_entity+'_hide_'+c not in st.session_state: st.session_state[select_entity+'_hide_'+c] = True
-                    chide = c2.toggle('hide',key=select_entity+'_hide_'+c)
-                    if chide:
-                        hide_columns.extend([c for c in r.table(input_table).columns if c not in columns])
+            track('start with columns')
+            for c in columns:
+                c1,c2 = st.columns([5,2],vertical_alignment="center")
+                cval = c1.text_input(c.replace(input_table+'_',''), key=select_entity+'_value_'+c,
+                                   help='Enter pattern, \*\* for all non-empty values, \*text\* to search anywhere, or YES/NO for flag fields')
+                if select_entity+'_show_'+c not in st.session_state: st.session_state[select_entity+'_show_'+c] = True
+                if select_entity+'_skip_'+c not in st.session_state: st.session_state[select_entity+'_skip_'+c] = False
+                cshow = c2.toggle('show',key=select_entity+'_show_'+c)
+                cskip = c2.toggle('skip',key=select_entity+'_skip_'+c)
+                if cval:
+                    if cskip:
+                        skip_datafields.update({c:MaybeUpper(cval)})
+                    else:
+                        select_datafields.update({c:MaybeUpper(cval)})
+                if not cshow:
+                    hide_columns.append(c)
 
-            final_processing.append({'table': input_table,
-                                     'segment': segment,
-                                     'hide': hide_segment,
-                                     'columns': columns,
-                                     'hide_columns': hide_columns,
-                                     'select_keys': select_keys,
-                                     'select_datafields': select_datafields,
-                                     'skip_datafields': skip_datafields})
-            track('end segment tab',segment)
+            irrelevant_columns = []
+            if segment=='base' and select_entity in entity_data_selector:  # base segments, for all but the system profiles
+                c = 'irrelevant fields'
+                c1,c2 = st.columns([5,4],vertical_alignment="center")
+                c1.write(c)
+                if select_entity+'_show'+c not in st.session_state: st.session_state[select_entity+'_show_'+c] = False
+                cshow = c2.toggle('show',key=select_entity+'_show_'+c)
+                if cshow:
+                    irrelevant_columns = [c for c in r.table(input_table).columns if c not in columns]
+
+        query_segments[segment].update({'table': input_table,
+                                 'segment': segment,
+                                 'show': show_segment,
+                                 'columns': columns+irrelevant_columns,
+                                 'hide_columns': hide_columns,
+                                 'select_keys': select_keys,
+                                 'select_datafields': select_datafields,
+                                 'skip_datafields': skip_datafields,
+                                 'filters': len(select_datafields)+len(skip_datafields)})
+
+        st.session_state['query_state'] = query_state
+        track('end segment tab',segment)
 
 input_table = ''
-for p in final_processing:
-    if not p['hide'] or p['select_datafields'] or p['skip_datafields']:
+for p in query_segments.values():
+    if not 'columns' in p: continue
+    if p['show'] or p['select_datafields'] or p['skip_datafields']:
         needed_columns = list(p['select_datafields'].keys())+list(p['skip_datafields'].keys())
-        if p['hide']:  # hide whole segment, but we need to do select/skip
+        if not p['show']:  # hide whole segment, but we need to do select/skip
             columns = needed_columns
         elif p['hide_columns']:
             columns = [c for c in p['columns'] if c not in p['hide_columns']]
@@ -191,7 +250,7 @@ for p in final_processing:
             df = df.join(r.table(p['table'])[columns])
         df = df.find(*p['select_keys'],**p['select_datafields']).skip(**p['skip_datafields'])
         if p['select_datafields'] or p['skip_datafields']:
-            if p['hide']:  # hide whole segment
+            if not p['show']:  # hide whole segment
                 drop_columns = r.table(p['table']).columns
             elif p['hide_columns']:
                 drop_columns = p['hide_columns']
@@ -203,14 +262,15 @@ for p in final_processing:
 track('frame ready')
 
 with header:
-    sum_select_datafields = {}
-    sum_skip_datafields = {}
-    for p in final_processing:
-        sum_select_datafields.update(p['select_datafields'])
-        sum_skip_datafields.update(p['skip_datafields'])
-
     st.header(f"pyracf: {select_entity}s")
     if input_table:
+        sum_select_datafields = {}
+        sum_skip_datafields = {}
+        for p in query_segments.values():
+            if 'select_datafields' in p:
+                sum_select_datafields.update(p['select_datafields'])
+            if 'skip_datafields' in p:
+                sum_skip_datafields.update(p['skip_datafields'])
         st.text(f'{df.shape[0]} records: {select_keys} {sum_select_datafields} not{sum_skip_datafields}')
 
 with output_frame:
